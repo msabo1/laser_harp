3517baeee4ff7314db85695f5fc1e324782c3e94
Mario Sabo <mariosabo2@gmail.com>
2019-04-13 17:05:03 +0200

    Scale settings to real model

--- laser_harp.ino
+++ laser_harp.ino
@@ -2,8 +2,8 @@
 
 //Define pins
 int ldr[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
-int laser[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-int speakerPin = 10;
+int laser[8] = {2, 3, 4, 6, 5, 7, 8, 9};
+int speakerPin = 11;
 
 //note matrix
 int note[50] = {NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1,
@@ -14,10 +14,10 @@ int note[50] = {NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1,
                 NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6,
                 NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7,
                 NOTE_C8};
-int midiNote[8] = {24, 26, 28, 29, 31, 33, 35, 36}
+int midiNote[8] = {24, 26, 28, 29, 31, 33, 35, 36};
 
-int ref = 800; //referent sensor value
-int octave = 3;
+int ref = 100; //referent sensor value
+int octave = 5;
 int output = 0;
 int mode = 0;
 int modeRefTime = 4000; //time to triger mode change
@@ -31,9 +31,10 @@ void play(int notePos, int oct = octave){
     Serial.write(midiNote[notePos] + oct*12);
     Serial.write(0x7F);
   }
+  delay(2);
 }
 
-void stopPlaying(int notePos, oct = octave){
+void stopPlaying(int notePos, int oct = octave){
   noTone(speakerPin);
   Serial.write(0x90);
   Serial.write(midiNote[notePos] + oct*12);
@@ -44,10 +45,9 @@ void playMode(){
   for (int i = 0; i < 8; i++){
     digitalWrite(laser[i], HIGH);
   }
-
   for(int i = 0; i < 8; i++){
     if(analogRead(ldr[i]) < ref){
-      play(i);
+      play(i, octave);
     }else{
       stopPlaying(i);
     }
@@ -76,6 +76,7 @@ void checkModeChange(int cmode){
     }
     //wait until hand is moved
     while(analogRead(ldr[cmode]) < ref){}
+    delay(20);
     //stop playing all notes
     for(int i = 0; i < 8; i++){
       stopPlaying(i);
@@ -93,8 +94,8 @@ void changeOctave(){
   for (int i = 0; i < 8; i++){
     digitalWrite(laser[i], HIGH);
   }
-
-  for(int i = 0; i < 8; i++){
+  delay(20);
+  for(int i = 0; i < 7; i++){
     if(analogRead(ldr[i]) < ref){
       play(i, i); //sound signal
       //turn off all other lasers
@@ -106,9 +107,12 @@ void changeOctave(){
       }
       //wait until hand is moved
       while(analogRead(ldr[i]) < ref){}
+      delay(10);
       octave = i;
       mode = 0;
+      break;
     }
+    delay(20);
   }
 }
 
@@ -117,12 +121,13 @@ void changeOutput(){
   digitalWrite(laser[1], HIGH);
   for(int i = 0; i < 2; i++){
     if(analogRead(ldr[i] < ref)){
-      play(i);
+      play(i, octave);
       digitalWrite(laser[(i + 1) % 2], LOW);
       while(analogRead(ldr[i] < ref)){}
       output = i;
       mode = 0;
     }
+    delay(10);
   }
 }
 
@@ -132,13 +137,17 @@ void setup() {
     pinMode(laser[i], OUTPUT);
   }
   pinMode(speakerPin, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
   if(!mode){
     for(int cmode = 1; cmode < 4; cmode++){
+      int cmode2 = mode;
       checkModeChange(cmode);
+      if(mode != cmode2){
+        break;
+      }
     }
   }
 
